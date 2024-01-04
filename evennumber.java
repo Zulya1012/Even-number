6a591ef8e69d08313280e85fdc464076e76daa1f
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-01-04 17:24:53 +0400

    Update evennumber.java

--- evennumber.java
+++ evennumber.java
@@ -1,10 +1,10 @@
-public class evennumber{
-	public static void main(String[] arg){
-		for (int i=1; i<=100; i++){
-			if (i % 2 == 1){
-			continue;
-}
-		System.out.println(i);
-}
-}
+public class evennumber {
+  public static void main(String[] arg) {
+    for (int i = 1; i <= 100; i++) {
+      if (i % 2 == 1) {
+        continue;
+      }
+      System.out.println(i);
+    }
+  }
 }
